0a2622a2eaed37e6f03fc86d88a7dea3d8553d52
KRISHNA <kraggr2909@gmail.com>
2026-09-20 22:53:27 +0530

    Best Time to Buy and Sell Stock - 2026-09-20T17:23:23.588Z

--- 121-best-time-to-buy-and-sell-stock/solution.java
+++ 121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,14 +1,6 @@
-// 2 ms | 62.2 MB
+// N/A | N/A
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = prices[0],max = 0; 
-
-        for(int i = 1; i< prices.length ; i++){
-            max = Math.max(max, prices[i] - min);
-            min = Math.min(min, prices[i]);
-        }
         
-
-        return max;
     }
 }
