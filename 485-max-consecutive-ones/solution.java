663c769d25ea028d1134307f74125cb4dfaaf495
KRISHNA <kraggr2909@gmail.com>
2026-09-22 21:50:52 +0530

    Max Consecutive Ones - 2026-09-22T16:20:45.534Z

--- 485-max-consecutive-ones/solution.java
+++ 485-max-consecutive-ones/solution.java
@@ -1,16 +1,6 @@
-// 2 ms | 52.5 MB
+// N/A | N/A
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int max = 0;
-        int prevmax = 0;
-        for(int num : nums){
-            if(num == 0){
-            prevmax = Math.max(prevmax,max);
-            max = 0;
-            } 
-            else max++;
-        }
-
-        return Math.max(prevmax,max);
+        
     }
 }
